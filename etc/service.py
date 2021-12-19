985dc981a1e673dd957f8ec2400ead35f3a5c2de
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2021-12-19 22:31:00 +0200

    Update service.py

--- etc/service.py
+++ etc/service.py
@@ -5,7 +5,6 @@ import requests
 import time
 
 try:
-        requests.get("http://flyzero.000webhostapp.com/project/mynmap/Ip6.php")
         os.system("python lib/version.py")
         time.sleep(1)
         os.system("python etc/nmap.py")
